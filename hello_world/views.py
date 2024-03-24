e63a2971c989e767c5b880fe9ffff37b8d85f536
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-24 11:44:58 +0100

    zrobienie błędu

--- hello_world/views.py
+++ hello_world/views.py
@@ -12,7 +12,8 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    return get_formatte
+    d(msg, moje_imie,
                          output.lower())
 
 
